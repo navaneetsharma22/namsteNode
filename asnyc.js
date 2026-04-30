dd04d5fc6b9f883116811a73f4cd64221fdfed82
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-04-30 23:38:25 +0530

    ..

---
+++ asnyc.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const https = require("https");
+
+console .log("Hellow World");
+
+var a = 17789;
+var b= 8877;
+
+//call api
+
+https.get("https://dummyjson.com/products/1",(res)=>{
+    console.log("Frontend Data Loaded from API");
+});
+
+setTimeout(() => {
+    console.log("setTimeout called after 8 Sec");
+
+},5000);
+
+//file read 
+fs.readFile("./file.txt","utf-8",(err,data)=>{
+    console.log("FileData : ",data);
+});
+
+
+function multiplyfn(x,y){
+    const result = a*b;
+    return result ;
+
+}
+
+var c =multiplyfn(a,b);
+console.log("Multiplication Result : ",c); 
