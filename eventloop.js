41e3adeefc76d397508d38f4344c43b2273ac540
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-03 17:58:18 +0530

    feat: add initial implementation of event loop functions and file reading

--- eventloop.js
+++ eventloop.js
@@ -1,1 +1,23 @@
-const fs = require 
+const fs = require ("fs");
+
+const a= 100;
+
+
+setImmediate(()=> console.log("This is a message from setImmediate function")); 
+
+fs.readFile("./file.txt" , "utf8" , () =>{
+    console.log("This is a message from readFile callback function");
+});
+
+setTimeout(()=>{
+    console.log("This is a message from setTimeout function");
+},0);
+
+function printA(){
+    console.log("a=" ,a);
+
+}
+
+printA();
+console.log("this is the last message");
+
