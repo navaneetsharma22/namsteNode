49fc58a3d6759760ec56dbf022a0aab0a4db0e64
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-19 00:33:20 +0530

    feat: refactor database connection setup and improve main function structure

--- database.js
+++ database.js
@@ -1,1 +1,27 @@
-const URL = "mongodb+srv://ksharma112288_db_user:aSyJS0uQeFAwthZH@database.o67zro2.mongodb.net/";
+const {MongoClient} = require("mongodb");
+
+const url = "mongodb+srv://ksharma112288_db_user:aSyJS0uQeFAwthZH@database.o67zro2.mongodb.net/";
+
+const client = new MongoClient(url);
+
+const dbName = "NamsteNode";
+
+async function main () {
+
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+
+    const collection = db.collection("users");
+
+
+    return "done.";
+
+
+}.close
+
+main() 
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+    
